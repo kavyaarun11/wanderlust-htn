f701b0f9db64fbcce49e15523357fa763031f5ff
kavyaarun11 <kavyaarun@gmail.com>
2021-09-18 00:05:57 -0400

    added firebase

---
+++ wanderlust-htn/src/auth/fire.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+const { REACT_APP_MY_API, REACT_APP_AUTHDOMAIN, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET, REACT_APP_MESSAGING_SENDER_ID, REACT_APP_APP_ID } = process.env;
+
+const firebaseConfig = {
+    apiKey: REACT_APP_MY_API,
+    authDomain: REACT_APP_AUTHDOMAIN,
+    projectId: REACT_APP_PROJECT_ID,
+    storageBucket: REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+    appId: REACT_APP_APP_ID
+  };
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;
