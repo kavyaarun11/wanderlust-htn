ffbec779d90ccef686d309416474c87b700191e8
kavyaarun11 <kavyaarun@gmail.com>
2021-09-18 11:58:05 -0400

    card styling

--- wanderlust-htn/src/Components/Card.js
+++ wanderlust-htn/src/Components/Card.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import "./Card.css";
-import {  createTheme } from '@material-ui/core/styles';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 
 const theme = createTheme({
   typography: {
@@ -16,21 +18,36 @@ const theme = createTheme({
   },});
 
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 500,
+    margin: 'auto',
+    
+  },
+  media: {
+    height: 150,
+    background: 'black',
+  
+
+  },
+  text:{
+    padding: 20,
+  }
+});
 
 export default function MediaCard() {
-
+  const classes = useStyles();
 
   return (
-<Card className="card-container">
-            <CardActionArea className = "card-action-area">
+<Card className={classes.root}>
+            <CardActionArea className = {classes.container} id="cardFrame">
 
-        <CardContent  className="card-content">
+        <CardContent  className="CardBox1">
           <Typography gutterBottom variant="h5" component="h2" className = "Cardcontent">
-            New Trip
+            {/* <b>Create New Trip</b> */}
           </Typography>
         </CardContent>
       </CardActionArea>
-
       </Card>
   );
 }
--- wanderlust-htn/src/Components/Card2.js
+++ wanderlust-htn/src/Components/Card2.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import "./Card.css";
-import {createTheme } from '@material-ui/core/styles';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 
 const theme = createTheme({
   typography: {
@@ -16,19 +18,44 @@ const theme = createTheme({
   },});
 
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 500,
+    margin: 'auto',
+    
+  },
+  media: {
+    height: 150,
+    background: 'black',
+  },
+
+  text:{
+    padding: 20,
+  }
+
+});
 
 export default function MediaCard2() {
+  const classes = useStyles();
 
   return (
-<Card className="card-container">
-            <CardActionArea className = "card-action-area">
+    <Card className={classes.root}>
+            <CardActionArea className = {classes.container} >
 
-        <CardContent  className="card-content">
+        <CardContent className="CardBox2">
           <Typography gutterBottom variant="h5" component="h2" className = "Cardcontent">
-            Past Trips
+            {/* <b>Previous Trips</b> */}
           </Typography>
         </CardContent>
       </CardActionArea>
+      {/* <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions> */}
       </Card>
       );
       }
